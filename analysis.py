c0782a8e4123a31d64ea81e359ff613e47a83bf8
LIS351-Hina <hsuzuki2@wisc.edu>
2025-04-29 10:20:17 -0700

    edited

--- analysis.py
+++ analysis.py
@@ -9,6 +9,9 @@ df = pd.read_csv(data_path)
 california_df = df[df["SFA_STATE"] == "CA"]
 california_df.to_csv("california_data.csv", index=False)
 
+num_rows = california_df.shape[0]
+print(f"Number of CA SFAs: {num_rows}")
+
 # Create a new CSV with only data about whether schools are using local food in the National School Lunch Program
 NSL_columns_to_include = [
     "SFA_NAME", "Q7_A1C1", "Q7_A1C2", "Q7_A1C3", "Q7_A1C4"
@@ -16,10 +19,10 @@ NSL_columns_to_include = [
 NSL_df = california_df[NSL_columns_to_include]
 
 NSL_new_column_names = {
-        "Q7_A1C1": "Never",  # Replace with your desired column name
-        "Q7_A1C2": "Did before SY 2022-23",  # Replace with your desired column name
-        "Q7_A1C3": "Did in SY 2022-23",  # Replace with your desired column name
-        "Q7_A1C4": "Doing in SY 2023-24"   # Replace with your desired column name
+        "Q7_A1C1": "Never",  
+        "Q7_A1C2": "Did before SY 2022-23",  
+        "Q7_A1C3": "Did in SY 2022-23", 
+        "Q7_A1C4": "Doing in SY 2023-24"   
     }
 
 NSL_df.rename(columns=NSL_new_column_names, inplace=True)
@@ -27,7 +30,7 @@ NSL_df.replace({0.0: 'no', 1.0: 'yes'}, inplace=True)
 NSL_df.to_csv("local-food-in-NSL.csv", index=False)
 df = pd.read_csv("local-food-in-NSL.csv")
 # How many SFAs said they have never used local food in their NSLP?
-yes_count = df["Never"].value_counts().get("yes", 0)  # Default to 0 if "yes" is not present
-print(f"Number of people who said 'yes' for Never: {yes_count}")
+yes_count = df["Never"].value_counts().get("yes", 0)  
+print(f"Number of SFAs that have never served local produce in their lunch: {yes_count}")
 
 
