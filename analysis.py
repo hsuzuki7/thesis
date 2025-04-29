46a1e6f12c687dec0187f96c52aab3ea7dac19bb
LIS351-Hina <hsuzuki2@wisc.edu>
2025-04-29 11:59:34 -0700

    edited

--- analysis.py
+++ analysis.py
@@ -10,7 +10,15 @@ california_df = df[df["SFA_STATE"] == "CA"]
 california_df.to_csv("california_data.csv", index=False)
 
 num_rows = california_df.shape[0]
-print(f"Number of CA SFAs: {num_rows}")
+print(f"Number of CA SFAs that answered to the survey: {num_rows}")
+
+# Count the numbers under the columns
+num_schools_sum = california_df['NUM_SCHOOLS'].sum()
+num_students_sum = california_df['NUM_STUDENTS'].sum()
+
+# Print the counts
+print(f"Count of NUM_SCHOOLS: {num_schools_sum}")
+print(f"Count of NUM_STUDENTS: {num_students_sum}")
 
 # Create a new CSV with only data about whether schools are using local food in the National School Lunch Program
 NSL_columns_to_include = [
@@ -29,8 +37,11 @@ NSL_df.rename(columns=NSL_new_column_names, inplace=True)
 NSL_df.replace({0.0: 'no', 1.0: 'yes'}, inplace=True)
 NSL_df.to_csv("local-food-in-NSL.csv", index=False)
 df = pd.read_csv("local-food-in-NSL.csv")
+
 # How many SFAs said they have never used local food in their NSLP?
 yes_count = df["Never"].value_counts().get("yes", 0)  
+no_count = df["Never"].value_counts().get("no", 0)  
 print(f"Number of SFAs that have never served local produce in their lunch: {yes_count}")
+print(f"Number of SFAs that have served local produce in their lunch: {no_count}")
 
 
